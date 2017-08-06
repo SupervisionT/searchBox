47df185dc1dfe7a0a59d8390d570cde2f52af37d
SupervisionT <a.alshaikh82@gmail.com>
2017-08-06 13:28:39 +0300

    fix icon req

--- src/plg-icon.js
+++ src/plg-icon.js
@@ -1,5 +1,5 @@
 var fs = require('fs');
-var index = fs.readFileSync(__dirname + '/search.png');
+var index = fs.readFileSync(__dirname + '/point_icon.png');
 module.exports = function(req, res) {
   res.writeHead(200, {'Content-Type': 'image/x-icon'} );
   res.end(index)
--- src/router.js
+++ src/router.js
@@ -4,6 +4,9 @@ module.exports = {
     'GET /pelias-leaflet-geocoder.js': require('./plg.js'),
     'GET /css/pelias-leaflet-geocoder.css': require('./plg-css.js'),
     'GET /images/point_icon.png': require('./plg-icon.js'),
+    'GET /css/images/search.png': require('./plg-search.js'),
+    'GET /images/polygon_icon.png': require('./plg-point-icon.js'),
+    // 'GET /images/point_icon.png': require('./plg-polygon-icon.js'),
     'POST /Coverege': require('./coverege.js')
 
     // 'POST /checkCov': require('./backend/getdata.js')
